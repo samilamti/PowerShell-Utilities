63809fbf93013c615bd9adc5e4f13fd53c22d6a5
Sami Lamti <sami.lamti@gmail.com>
2022-06-06 22:03:14 +0100

    Create Require-Admin.ps1

---
+++ Require-Admin.ps1
@@ -0,0 +1,4 @@
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+If (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Throw "Please run this script as Administrator"
+}
